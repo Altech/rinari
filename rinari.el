5b440eb180c0259ab8946fd5eab89418789bd454
Altech <takeno.sh@gmail.com>
2013-11-09 17:14:58 +0900

    Add command to find routes.rb

--- rinari.el
+++ rinari.el
@@ -780,6 +780,7 @@ and redirects."
    (environment "e" ((t . "config/environments/")) nil)
    (application "a" ((t . "config/application.rb")) nil)
    (configuration "n" ((t . "config/")) nil)
+   (routes "R" ((t . "config/routes.rb")) nil)
    (bin "b" ((t . "bin/")) nil)
    (lib "l" ((t . "lib/")) nil)
    (log "o" ((t . "log/")) nil)
